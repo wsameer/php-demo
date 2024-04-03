84d69a3e7277c2f3f31200fcc164835c3507a6cf
wsameer <dev.wsameer@gmail.com>
2024-04-03 17:15:33 -0400

    refactor: relocated code to delete a note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,30 +7,13 @@ $db = new Database($config['database'], $config['user'], $config['password']);
 
 $currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+  'id' => $_GET['id']
+])->findOrFail();
 
-  $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
-  ])->findOrFail();
+authorize($note['user_id'] === $currentUserId);
 
-  authorize($note['user_id'] === $currentUserId);
-
-  $db->query('DELETE FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
-  ]);
-
-  header('location: /notes');
-  exit();
-
-} else {
-  $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
-  ])->findOrFail();
-
-  authorize($note['user_id'] === $currentUserId);
-
-  view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-  ]);
-}
+view("notes/show.view.php", [
+  'heading' => 'Note',
+  'note' => $note
+]);
