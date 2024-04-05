15906c58fd58cb44a4894381f8ff9d836d06aed6
wsameer <dev.wsameer@gmail.com>
2024-04-04 21:15:04 -0400

    refactors show note logic

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,16 +5,21 @@
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <p>
-      <a href="/notes" class="text-blue-500 underline">Go back to all notes</a>
+      <a href="/notes" class="text-blue-500 underline">
+        Go back to all notes
+      </a>
     </p>
     
     <p class="mt-4"><?= htmlspecialchars($note['body']) ?></p>
 
-    <form class="mt-6" method="POST">
-      <input type="hidden" name="_method" value="DELETE">
-      <input type="hidden" id="id" name="id" value="<?= $note['id'] ?>">
-      <button class="text-sm text-red-500">Delete</button>
-    </form>
+    <footer class="mt-6">
+      <a
+        href="/note/edit?id=<?= $note['id'] ?>"
+        class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        Edit
+      </a>
+    </footer>
   </div>
 </main>
 
