0d6ea83102d98b24eec1537931ec106a255969e2
wsameer <dev.wsameer@gmail.com>
2024-04-06 14:17:50 -0400

    store user information after login

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -39,12 +39,10 @@ if ($user) {
 } else {
   $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
     'email' => $email,
-    'password' => $password
+    'password' => password_hash($password, PASSWORD_BCRYPT)
   ]);
 
-  $_SESSION['user'] = [
-    'email' => $email
-  ];
+  login($user);
 
   header('location: /');
   exit();
