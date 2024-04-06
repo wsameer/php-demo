ed279433bdae95e6b655c070db5ad87dee3cf1f6
wsameer <dev.wsameer@gmail.com>
2024-04-05 23:12:51 -0400

    updates route method to support middleware

--- Core/router.php
+++ Core/router.php
@@ -2,45 +2,56 @@
 
 namespace Core;
 
-use Core\Response;
+use Core\Middleware\Auth;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
 
 final class Router {
 
   protected $routes = [];
 
-  private function add($method, $uri, $controller) {
+  private function add($method, $uri, $controller, $middleware = null) {
     // $this->routes[] = compact($uri, $controller, $method);
     // same as above
     $this->routes[] = [
       'uri' => $uri,
       'controller' => $controller,
-      'method' => $method
+      'method' => $method,
+      'middleware' => $middleware
     ];
+
+    return $this;
   }
 
   public function get($uri, $controller) {
-    $this->add('GET', $uri, $controller);
+    return $this->add('GET', $uri, $controller);
   }
 
   public function post($uri, $controller) {
-    $this->add('POST', $uri, $controller);
+    return $this->add('POST', $uri, $controller);
   }
 
   public function put($uri, $controller) {
-    $this->add('PUT', $uri, $controller);
+    return $this->add('PUT', $uri, $controller);
   }
 
   public function patch($uri, $controller) {
-    $this->add('PATCH', $uri, $controller);
+    return $this->add('PATCH', $uri, $controller);
   }
 
   public function delete($uri, $controller) {
-    $this->add('DELETE', $uri, $controller);
+    return $this->add('DELETE', $uri, $controller);
+  }
+
+  public function only($key) {
+    $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+    return $this;
   }
 
   public function route($uri, $method) {
     foreach ($this->routes as $route) {
       if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+        Middleware::resolve($route['middleware']);
         return require base_path($route['controller']);
       }
     }
