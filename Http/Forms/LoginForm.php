ba4267847c2d132690d993e121b225c5f2a54278
wsameer <dev.wsameer@gmail.com>
2024-04-08 23:36:13 -0400

    adds login form validation class

---
+++ Http/Forms/LoginForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\ValidationException;
+use Core\Validator;
+
+final class LoginForm {
+  protected $errors = [];
+
+  public function __construct(public array $attributes) {
+    if (!Validator::email($attributes['email'])) {
+      $this->errors['email'] = 'Please provide a valid email address.';
+    }
+
+    if (!Validator::string($attributes['password'])) {
+      $this->errors['password'] = 'Please provide a valid password.';
+    }
+  }
+
+  public static function validate($attributes) {
+    $instance = new static($attributes);
+    return $instance->failed() ? $instance->throw() : $instance;
+  }
+
+  public function throw() {
+    ValidationException::throw($this->getErrors(), $this->attributes);
+  }
+
+  public function getErrors() {
+    return $this->errors;
+  }
+
+  public function setError($field, $message = '') {
+    $this->errors[$field] = $message;
+    return $this;
+  }
+
+  public function failed() {
+    return count($this->errors);
+  }
+
+  public function error($field, $message) {
+    $this->errors[$field] = $message;
+    return $this;
+  }
+}
