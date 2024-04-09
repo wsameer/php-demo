ffdb70f6b501dd14c13ebd4afea9e1ee657044a7
wsameer <dev.wsameer@gmail.com>
2024-04-08 20:37:23 -0400

    creates Session class

---
+++ Core/Session.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core;
+
+final class Session {
+  
+  public static function get($key, $default = null) {
+    return $_SESSION['_flash'][$key] ?? $_SESSION[$key] ?? $default;
+  }
+
+  public static function flash($key, $value) {
+    $_SESSION['_flash'][$key] = $value;
+  }
+
+  public static function unFlash($key) {
+    unset($_SESSION['_flash']);
+  }
+
+  public static function flush() {
+    $_SESSION = [];
+  }
+
+  public static function destroy() {
+    static::flush();
+
+    session_destroy();
+
+    $params = session_get_cookie_params();
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+  }
+
+  public static function has($key) {
+    return (bool) static::get($key);
+  }
+}
